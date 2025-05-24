78da9d740f43785babaedf243d83f1ff0a8fa79b
Jeki <ahmadzaqi490@gmail.com>
2025-05-25 06:34:54 +0700

    update route

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -17,7 +17,7 @@ class CheckRole
     public function handle(Request $request, Closure $next, ...$roles): Response
     {
         if (!Auth::check()) {
-            return redirect('/login');
+            return redirect()->route('login');
         }
 
         $user = Auth::user();
