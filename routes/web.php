bad96d2c5b3ea465e7b54ce6ed33454f44aace21
Jeki <ahmadzaqi490@gmail.com>
2025-05-25 21:26:39 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('/', [App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
+    Route::get('/', [App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login-form');
     Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login');
     
     Route::get('/register', [App\Http\Controllers\AuthController::class, 'showRegisterForm'])->name('register');
